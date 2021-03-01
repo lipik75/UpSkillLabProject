afa4d9d4faf907a83e26a156f12038161e6b7802
Sivukha Ihar <lipik75@gmail.com>
2021-03-01 23:21:27 +0300

    Add Junit method testSolve

---
+++ src/test/java/TestSolve.java
@@ -0,0 +1,22 @@
+import junit.Equation;
+import org.junit.Assert;
+import org.junit.Assert.*;
+import org.junit.Test;
+
+
+/**
+ * Test of solve method, of class Equation.
+ */
+public class TestSolve {
+    @Test
+    public void testSolve(){
+        Equation equation = new Equation();
+
+        double[] result = equation.solve(1, -3, 5);
+        double[] expectedResult = {-1, 4};
+
+        Assert.assertArrayEquals(expectedResult, result, 5);
+
+
+    }
+}
