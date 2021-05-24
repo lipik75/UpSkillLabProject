9648237f31b1d6d31e42ba3b065d32b775388927
Ihar Sivukha <62198481+lipik75@users.noreply.github.com>
2021-05-24 18:41:49 +0300

    Update Equation.java

--- src/main/java/junit/Equation.java
+++ src/main/java/junit/Equation.java
@@ -25,7 +25,7 @@ public class Equation {
         } else {
             System.out.println("Error: division by zero.");
         }
-        System.out.println("Finish");
+        System.out.println("Finish...");
 
         return results;
     }
